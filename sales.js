0fdc498f53adf4afd768d75665ad3ec4759da657
Tommy Works <93413471+7ommyBoy@users.noreply.github.com>
2022-10-03 12:56:58 -0700

    Update sales.js

--- sales.js
+++ sales.js
@@ -1,51 +1,52 @@
 "use strict";
-// Arrays of region
-function analyseData() {
+// Activity 5 HW JavaScript
+// Tommy Works Jr. 
+// 10/03/2022 
+
+
+// The date for the sales 
 const region1 = [1540, 1130, 1580, 1105];
 const region2 = [2010, 1168, 2305, 4102];
 const region3 = [2450, 1847, 2710, 2391];
 const region4 = [1845, 1491, 1284, 1575];
 const region5 = [2120, 1767, 1599, 3888];
 
-//calculating quarterly sales
-const q1 = region1[0] + region2 [0]+ region3 [0] + region4 [0] + region5 [0]; 
-const q2 = region1[1] + region2 [1]+ region3 [1] + region4 [1] + region5 [1]; 
-const q3 = region1[2] + region2 [2]+ region3 [2] + region4 [2] + region5 [2]; 
-const q4 = region1[3] + region2 [3]+ region3 [3] + region4 [3] + region5 [3]; 
-
-//calculating region wise sales
-
-const reg1Sale=getSale(region1);
-const reg2Sale=getSale(region2);
-const reg3Sale=getSale(region3);
-const reg4Sale=getSale(region4);
-const reg5Sale=getSale(region5);
-document.write(output);
-
-//calculating total sales
-const toatlSales= q1 + q2 + q3 + q4;
-
-// Outputing 
-const output = "Sales by Quarter/n";
-output = output + `Q1: `+q1+ `\nQ2: ` +q2+ `\nQ2: ` +q3+ `\nQ2: ` +q4;
-document.write(output);
-
-
-//Outputting Sales By Region's
-output = "Sales by Region \n";
-output = output+ `Region 1: ` +reg1Sale+ `\nRegion 2: ` +reg2Sale+ `\nRegion 3: ` +reg3Sale+ `\nRegion 4: ` +reg4Sale+ `\nRegion 5: ` +reg5Sale;
-document.write(output);
-
-output ="Total Sales: "
-output = output+toatlSales;
-document.write(output);
+// The calculation of the totals quarter 
+const quarterTotals = [0, 0, 0, 0];
+// Iterating through the array +1
+for (let i = 0; i <quarterTotals.length; i++) {
+    quarterTotals[i] = region1[i] + region2[i] + region3[i] + region4[i] + region5[i];
+}
+// This will display the totals in the quarter
+document.write("<h2> Sales by the Quarter</h2>");
+// Iterating through the array +1
+for (let i = 0; i< quarterTotals.length; i++) {
+    document.write(`Q${i + 1} : $${quarterTotals[i]}<br>`);
+}
+// This will caculate the total's region
+const regionTotals = [0, 0, 0, 0, 0];
+// Iterating through the array +1 
+for (let i = 0; i <quarterTotals.length; i++) {
+    regionTotals[0] += region1[i];
+    regionTotals[1] += region2[i];
+    regionTotals[2] += region3[i];
+    regionTotals[3] += region4[i];
+    regionTotals[4] += region5[i];
+}
+// this will display the totals by quarter
+document.write ("<h2> Sales by Region </h2>");
+// Iterating through the array +1 
+for ( let i = 0; i <regionTotals.length; i++) {
+    document.write(`Region ${i + 1}: $${regionTotals[i]}<br>`);
 }
 
-//Functions to get the sum of array elements 
-function getSale(array) {
-    sum = 0;
-    for (index = 0; index <array.length; index++) {
-        sum = sum+array[index];
-    }
-    return sum; 
+// This will caculate the Total Sales 
+let totalSales = 0;
+for ( let i = 0; i < quarterTotals.length; i++) {
+    totalSales += quarterTotals[i];
 }
+
+// This will output the Total sales
+document.write("<h2> Total Sales </h2>");
+document.write(`$${totalSales}`);
+// End
